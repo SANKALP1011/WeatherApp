9ed68c977725b719150b8c17816c81262ee5f569
SANKALP1011 <tigerishan426@gmail.com>
2021-11-19 11:28:28 +0530

    Express , https and nodemon added.

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const https = require("https");
 const app = express();
+const ejs = require("ejs");
+app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(__dirname+'/public'));
 
 app.get("/",function(req,res){
-    res.sendFile(__dirname+"/index.html");
+    res.render("weather");
 })
-app.listen("3002",function(){
+app.post("/",function(req,res){
+   const url = "https://api.openweathermap.org/data/2.5/weather?q=Lucknow&appid=8045b99916c2e151cb2114bdf1d26663";
+   https.get(url,function(response){
+       response.on("data",function(data){
+           const weatherInfo = JSON.parse(data);
+           console.log(weatherInfo);
+       })
+   })
+})
+
+app.listen("3003",function(){
     console.log("server is up and running");
 })
 
