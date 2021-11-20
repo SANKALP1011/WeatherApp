0805177ef667a14e61bd79883a6a775553d2942a
SANKALP1011 <tigerishan426@gmail.com>
2021-11-20 20:17:28 +0530

    Data from api fetched , implemented using ejs.

--- app.js
+++ app.js
@@ -6,20 +6,50 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(__dirname+'/public'));
 
+var CityName = "";
+var temp = 0.0;
+var TempFeelsLike = 0.0;
+var MinTemp = 0.0;
+var MaxTemp = 0.0;
+var Pressure = 0;
+var Humidity = 0;
+var CloudCondition = "";
+
 app.get("/",function(req,res){
-    res.render("weather");
+    res.render("weather",{CityName: CityName});
 })
 app.post("/",function(req,res){
   var city = req.body.CityName;
   console.log(city);
-   const url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=8045b99916c2e151cb2114bdf1d26663";
+   const url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&appid=8045b99916c2e151cb2114bdf1d26663";
    https.get(url,function(response){
        response.on("data",function(data){
            const weatherInfo = JSON.parse(data);
-           console.log(weatherInfo);
-           console.log(req.body.CityName)
+           CityName = req.body.CityName;
+           temp = weatherInfo.main.temp;
+           TempFeelsLike = weatherInfo.main.feels_like;
+           MinTemp = weatherInfo.main.temp_min;
+           MaxTemp = weatherInfo.main.temp_max;
+           Pressure = weatherInfo.main.pressure;
+           Humidity = weatherInfo.main.humidity;
+           CloudCondition = weatherInfo.weather[0].description;
+
+           console.log(CityName);
+           console.log(temp);
+           res.render("weather",{CityName: CityName ,
+             Temp: temp,
+             TempFeelsLike: TempFeelsLike,
+             MinTemp: MinTemp,
+             MaxTemp: MaxTemp,
+             Pressure: Pressure,
+             Humidity: Humidity,
+             Cloud: CloudCondition
+            
+            });
+
        })
    })
+   
 })
 
 app.listen("3003",function(){
